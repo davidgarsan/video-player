9a70564c8bcd2f70d220de28045427909cc2138f
David García Sánchez <davidgarsan@gmail.com>
2017-06-05 22:22:52 +0200

    FIX mixed content warning (HTTPS)

--- src/utils/medias.js
+++ src/utils/medias.js
@@ -1,7 +1,7 @@
 export const DEFAULT_MOVIE_ID = '9761';
 export const Medias = {
   movie_9761: {
-    dash: 'http://media.axprod.net/dash/ED_TTML_NEW/Clear/Manifest_sub_in.mpd',
+    dash: 'https://media.axprod.net/dash/ED_TTML_NEW/Clear/Manifest_sub_in.mpd',
     hls: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
   },
 };
